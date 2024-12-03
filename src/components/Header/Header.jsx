ab8a9a48b556ded785b090ebfa2eee5204fd2221
Bartek <kuzniewski.bar@gmail.com>
2024-12-03 14:41:48 +0100

    move header code from home page to header component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,34 @@
+import { Wrapper } from '../Wrapper/Wrapper';
+import styles from './header.module.css'
+
+import headerImage from '../../assets/header-img.svg';
+
+export function Header() {
+	return (
+		<header className={styles.header}>
+			<Wrapper>
+				<div className={styles.headerContent}>
+					<div className={styles.headerText}>
+						<h2>ZOSTAŃ KRÓLEM SZACHÓW</h2>
+						<h3>
+							CHCESZ RZĄDZIĆ SZACHOWNICĄ I WYGRYWAĆ WSZYSTKIE BITWY NICZYM
+							JAGIEŁŁO POD GRUNWALDEM?
+						</h3>
+						<p>
+							Zobacz profesjonalne kursy, które przygotowaliśmy specjalnie dla
+							Ciebie. Bez względu na to czy dopiero zaczynasz, czy może masz już
+							pierwsze zwycięstwa na szachowym polu bitwy.
+						</p>
+						<button className={styles.button}>zobacz więcej</button>
+					</div>
+					<div className={styles.headerImg}>
+						<img
+							src={headerImage}
+							alt='Król siedzący na tronie w otoczeniu pionów szachowych'
+						/>
+					</div>
+				</div>
+			</Wrapper>
+		</header>
+	);
+}
--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -1,37 +1,11 @@
+import { Header } from '../../components/Header/Header';
 import { Layout } from '../../components/Layout/Layout';
-import { Wrapper } from '../../components/Wrapper/Wrapper';
-import styles from './HomePage.module.css';
 
-import headerImage from '../../assets/header-img.svg';
 
 export function HomePage() {
 	return (
 		<Layout>
-			<header className={styles.header}>
-				<Wrapper>
-					<div className={styles.headerContent}>
-						<div className={styles.headerText}>
-							<h2>ZOSTAŃ KRÓLEM SZACHÓW</h2>
-							<h3>
-								CHCESZ RZĄDZIĆ SZACHOWNICĄ I WYGRYWAĆ WSZYSTKIE BITWY NICZYM
-								JAGIEŁŁO POD GRUNWALDEM?
-							</h3>
-							<p>
-								Zobacz profesjonalne kursy, które przygotowaliśmy specjalnie dla
-								Ciebie. Bez względu na to czy dopiero zaczynasz, czy może masz
-								już pierwsze zwycięstwa na szachowym polu bitwy.
-							</p>
-							<button className={styles.button}>zobacz więcej</button>
-						</div>
-						<div className={styles.headerImg}>
-							<img
-								src={headerImage}
-								alt='Król siedzący na tronie w otoczeniu pionów szachowych'
-							/>
-						</div>
-					</div>
-				</Wrapper>
-			</header>
+			<Header />
 		</Layout>
 	);
 }
