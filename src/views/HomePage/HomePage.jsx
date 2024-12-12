d274d99e3ebe6b856f135813cef46fbc74879b28
Bartek <kuzniewski.bar@gmail.com>
2024-12-12 08:51:16 +0100

    added trust us section

--- src/views/HomePage/HomePage.jsx
+++ src/views/HomePage/HomePage.jsx
@@ -2,6 +2,7 @@ import { Header } from '../../components/Header/Header';
 import { KnowUsBetter } from '../../components/KnowUsBetter/KnowUsBetter';
 import { Layout } from '../../components/Layout/Layout';
 import { OurCourses } from '../../components/OurCourses/OurCourses';
+import { TrustUs } from '../../components/TrustUs/TrustUs';
 
 import styles from './HomePage.module.css';
 
@@ -12,6 +13,7 @@ export function HomePage() {
 			<main className={styles.main}>
 				<OurCourses />
 				<KnowUsBetter />
+				<TrustUs />
 			</main>
 		</Layout>
 	);
