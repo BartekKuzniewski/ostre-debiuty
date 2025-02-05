23c355f466810b124b0c60f2f2d62c558be31725
Bartek <kuzniewski.bar@gmail.com>
2025-02-05 10:48:44 +0100

    added courses info section

--- src/views/CoursesPage/CoursesPage.jsx
+++ src/views/CoursesPage/CoursesPage.jsx
@@ -2,6 +2,7 @@ import { useParams } from 'react-router-dom';
 import { Layout } from '../../components/Layout/Layout';
 import { Wrapper } from '../../components/Wrapper/Wrapper';
 import { coursesData } from '../../constants/coursesData';
+import { FaRegEnvelope } from 'react-icons/fa';
 import styles from './CoursesPage.module.css';
 
 export function CoursesPage() {
@@ -15,7 +16,27 @@ export function CoursesPage() {
 		<Layout>
 			<main className={styles.coursesPage}>
 				<Wrapper>
-					<h2>{course.name}</h2>
+					<div className={styles.container}>
+						<div className={styles.imageSection}>
+							<img
+								src={course.image}
+								alt={course.name}
+								className={styles.courseImage}
+							/>
+							<h2 className={styles.courseTitle}>{course.name}</h2>
+						</div>
+						<div className={styles.detailsSection}>
+							<h3 className={styles.courseHeading}>{course.name}</h3>
+							<p className={styles.courseDescription}>{course.courseInfo}</p>
+							<div className={styles.purchaseSection}>
+								<span className={styles.delivery}>
+									<FaRegEnvelope /> Wysyłka natychmiastowa
+								</span>
+								<span className={styles.price}>{course.price} zł</span>
+								<button className={styles.addToCart}>DO KOSZYKA</button>
+							</div>
+						</div>
+					</div>
 				</Wrapper>
 			</main>
 		</Layout>
