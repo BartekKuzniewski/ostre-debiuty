3a1e6d933ecf9bf42ec35bbe7754b97695855c0b
Bartek <kuzniewski.bar@gmail.com>
2025-02-18 17:33:52 +0100

    made trust us section responsive

--- src/views/ShoppingCart/ShoppingCart.jsx
+++ src/views/ShoppingCart/ShoppingCart.jsx
@@ -25,54 +25,56 @@ export function ShoppingCart() {
 					{cart.length === 0 ? (
 						<p className={styles.emptyCart}>Twój koszyk jest pusty.</p>
 					) : (
-						cart.map((course) => (
-							<div key={course.id} className={styles.product}>
-								<div className={styles.leftContent}>
-									<div className={styles.imageSection}>
-										<img
-											src={course.image}
-											alt={course.title}
-											className={styles.courseImage}
-										/>
-										<h3 className={styles.courseTitle}>{course.name}</h3>
+						<>
+							{cart.map((course) => (
+								<div key={course.id} className={styles.product}>
+									<div className={styles.leftContent}>
+										<div className={styles.imageSection}>
+											<img
+												src={course.image}
+												alt={course.title}
+												className={styles.courseImage}
+											/>
+											<h3 className={styles.courseTitle}>{course.name}</h3>
+										</div>
+										<p> {course.name}</p>
 									</div>
-								</div>
-								<div className={styles.rightContent}>
-									<div className={styles.productPrice}>
+									<div className={styles.rightContent}>
 										<p>{course.price} zł</p>
-										<p onClick={() => removeFromCart(course.id)}>
-											<RiDeleteBin5Line />
+										<p onClick={() => removeFromCart(course.id)} className={styles.garbage}>
+											<RiDeleteBin5Line size='32px' />
 										</p>
 									</div>
 								</div>
-							</div>
-						))
-					)}
+							))}
 
-					<div className={styles.paymentSection}>
-						<h3>Wybierz formę płatności</h3>
-						<div className={styles.paymentContent}>
-							<div className={styles.paymentMethods}>
-								<Button className={styles.cartButton}>
-									<img src={blik} alt='BLIK' />
-								</Button>
-								<Button>
-									<img src={przelewy} alt='Przelewy24' />
-								</Button>
-								<Button>
-									<img src={PayPal} alt='PayPal' />
-								</Button>
-								<Button>
-									<img src={creditCard} alt='Karta' />
-								</Button>
+							<div className={styles.paymentSection}>
+								<h3>Wybierz formę płatności</h3>
+
+								<div className={styles.paymentContent}>
+									<div className={styles.paymentMethods}>
+										<Button className={styles.cartButton}>
+											<img src={blik} alt='BLIK' />
+										</Button>
+										<Button>
+											<img src={przelewy} alt='Przelewy24' />
+										</Button>
+										<Button>
+											<img src={PayPal} alt='PayPal' />
+										</Button>
+										<Button>
+											<img src={creditCard} alt='Karta' />
+										</Button>
+									</div>
+									<div className={styles.totalPrice}>
+										<p className={styles.price}>{totalPrice.toFixed(2)} zł</p>
+										<p className={styles.productsValue}>Wartość produktów</p>
+										<Button>Kupuję i płacę</Button>
+									</div>
+								</div>
 							</div>
-							<div className={styles.totalPrice}>
-								<p className={styles.price}>{totalPrice.toFixed(2)} zł</p>
-								<p className={styles.productsValue}>Wartość produktów</p>
-								<Button>Kupuję i płacę</Button>
-							</div>
-						</div>
-					</div>
+						</>
+					)}
 				</Wrapper>
 			</main>
 		</Layout>
