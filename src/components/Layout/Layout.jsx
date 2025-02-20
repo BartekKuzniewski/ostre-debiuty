781a23a65f89509407517e96c1ab486259426c3d
Bartek <kuzniewski.bar@gmail.com>
2025-02-20 10:19:05 +0100

    added feature that move to top of every page change

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,9 +1,22 @@
 import { Navbar } from '../Navbar/Navbar';
 import { Footer } from '../Footer/Footer';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const ScrollToTop = () => {
+	const location = useLocation();
+
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, [location]);
+
+	return null;
+};
 
 export function Layout({ children }) {
 	return (
 		<>
+			<ScrollToTop />
 			<Navbar />
 			{children}
 			<Footer />
